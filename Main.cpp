2b09050aefcbf28629bfd87738f4796b68f331ac
Explicitname <explicit6911@gmail.com>
2014-11-17 14:23:36 -0500

    Variable Type Problem

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,9 @@
 /*
  * Main.cpp
  *
- *  Created on: Nov 17, 2014
- *      Author: explicitname
+ *  Created on:  Nov 17, 2014
+ *      Author:  explicitname
+ *      purpose: To calculate simple arithmetic.
  */
 #include <iostream>
 using namespace std;
@@ -11,42 +12,55 @@ int main()
 
 {
 
-	float num1;
-	float num2;
-	float sum;
+// Variable Declaration
+
+	float num1, num2, sum;
 	char oper;
+	char cont = 'y';
 
-	cout << "Josh's Simple Calculator" << endl;
-	cout << "Sample Format: 1 + 1 or 2 - 2" << endl;
-	cout << "Please Enter Statement: " << endl;
-	cin >> num1 >> oper >> num2;
+// Flow Control - Continues until variable cont is not equal to 'y'
+	do {
+		cout << "Josh's Simple Calculator" << endl;
+		cout << "Sample Format: 1 + 1 or 2 - 2" << endl;
+		cout << "Please Enter Statement: " << endl;
+		cin >> num1 >> oper >> num2;
 
-	if (oper == '+') {
-		sum = num1 + num2;
-		cout << sum << endl;
 
-	}
+		if (num1 || num2 == char) {
+			cout << "Error! You must use numbers!" << endl;
+		}
 
-	else if (oper == '-') {
+		if (oper == '+') {
+			sum = num1 + num2;
+			cout << sum << endl;
+
+		}
+
+		else if (oper == '-') {
 			sum = num1 - num2;
 			cout << sum << endl;
-	}
-	else if (oper == '/') {
+		}
+		else if (oper == '/') {
 			sum = num1 / num2;
 			cout << sum << endl;
-	}
+		}
 
-	else if (oper == '*') {
+		else if (oper == '*') {
 			sum = num1 * num2;
 			cout << sum << endl;
 
-	}
+		}
 
-	else {
+		else {
 			cout << "Error!" << endl;
-	}
+			cont = 'n';
+		}
+		cout << "Would you like to continue? " << endl;
+		cout << "Enter y to continue or n to stop: ";
+		cin >> cont;
 
 
+	}	while (cont == 'y');
 
 
 
